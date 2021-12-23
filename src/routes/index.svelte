bcf18a5782f3ab2bd3746724d0c8c2252fd0501a
Steinberg <steinberg@hey.com>
2021-12-23 23:58:33 +0100

    Fix feature link (with js)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,29 @@
 <script>
 	import Dice from '../libs/css-dice/dice.svelte';
 	import AccentHue from '../libs/AccentHue.svelte';
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+
 	let hue = 358.7;
+	let js = 'no-js';
+
+	onMount(() => js = 'js');
 </script>
 
 <svelte:head>
 	<title>Some makeup</title>
 </svelte:head>
 
+<div on:click={() => goto('/dice')} class:js>
 <Dice />
 
 <p>See Dice</p>
+</div>
 
 <AccentHue hue={hue} />
+
+<style>
+	.js:hover {
+		cursor: pointer;
+	}
+</style>
