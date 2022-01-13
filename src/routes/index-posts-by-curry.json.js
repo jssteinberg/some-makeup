2bc6bdc29271ec7d2733abb15a34b8d837138cbb
Steinberg <steinberg@hey.com>
2022-01-13 21:51:29 +0100

    Cooler FP posts

--- src/routes/index-posts-by-curry.json.js
+++ src/routes/index-posts-by-curry.json.js
@@ -1,6 +1,6 @@
 const rootPages = () => {
 	const mdFiles = import.meta.globEager(`./*.md`)
-	const getSlug = (path) => path.replace(/.*\/(.*)\..*$/, "$1")
+	const getSlug = path => path.replace(/.*\/(.*)\..*$/, "$1")
 
 	return Object.keys(mdFiles)
 		.map(path => new Object({
@@ -10,42 +10,38 @@ const rootPages = () => {
 			title: mdFiles[path].metadata?.title || getSlug(path),
 			metadata: mdFiles[path].metadata,
 		}))
-		.filter(file => !["index"].includes(file.slug))
+		.filter(file => ![`index`].includes(file.slug))
 }
 
 export async function get({ params }) {
 	const { slug } = params
 	const pages = rootPages()
-	const getPropValFromObj = (obj, prop) =>
-		prop.split('.').reduce((acc, c) => acc && acc[c], obj)
-
-	const sublist = (page, notHas = false) => {
-		return prop =>
-			(notHas === `!`) || notHas ?
-				page.filter(doc => !getPropValFromObj(doc,prop)) :
-				page.filter(doc => getPropValFromObj(doc,prop))
+	const getProp = (obj, prop, i=null) => {
+		const val = prop.split('.').reduce((acc, c) => acc && acc[c], obj)
+		return val && typeof i === `number` ? val[i] : val
 	}
+	const listPosts = (list, hasProp=1) => (prop, i) => !hasProp ?
+		list
+			.filter(obj => !getProp(obj, prop, i))
+		: list
+			.filter(obj => getProp(obj, prop, i))
+			.sort((a, b) => organize(a, b, prop, i))
+	const organize = (a, b, prop, i=null) => {
+		const valA = getProp(a, prop, i)
+		const valB = getProp(b, prop, i)
 
-	const organize = (pageA, pageB, [propStr, index = null]) => {
-		const valA = typeof index === 'number' ?
-			getPropValFromObj(pageA,propStr)[index] :
-			getPropValFromObj(pageA,propStr)
-		const valB = typeof index === 'number' ?
-			getPropValFromObj(pageA,propStr)[index] :
-			getPropValFromObj(pageA,propStr)
-
+		if (valA instanceof Date)
+			return valA - valB
 		if (new Date(valA) instanceof Date)
 			return new Date(valA) - new Date(valB)
-		if (typeof valA === 'string')
-			return valA > valB ? -1 : valB > valA ? 1 : 0
+		if (typeof valA === `string`)
+			return valA.localCompare(valB)
 		return 0
 	}
 
 	return { status: 200, body: [
-		...sublist(pages)(`metadata.date`)
-		.sort((pageA, pageB) => organize(pageA, pageB, [`metadata.date`, 0]))
-		.reverse(),
-		...sublist(pages, `!`)(`metadata.date`)
-		.sort((pageA, pageB) => organize(pageA, pageB, [`title`]))
+		...listPosts(pages)(`metadata.date`, 0).reverse(),
+		...listPosts(pages, false)(`metadata.date`)
+			.sort((a, b) => organize(a, b, `title`))
 	]}
 }
