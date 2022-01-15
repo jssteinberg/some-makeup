061fce21e5893fec5238c396dd126dc678dc9f8b
Johan S. Steinberg <steinberg@hey.com>
2022-01-15 15:03:25 +0100

    Mv list and sorting of posts to util file

--- src/routes/index-posts-by-curry.json.js
+++ src/routes/index-posts-by-curry.json.js
@@ -1,6 +1,8 @@
+import getPostsFrom from '../libs/utils/getPosts.js';
+
 const rootPages = () => {
-	const mdFiles = import.meta.globEager(`./*.md`)
-	const getSlug = path => path.replace(/.*\/(.*)\..*$/, "$1")
+	const mdFiles = import.meta.globEager(`./*.md`);
+	const getSlug = path => path.replace(/.*\/(.*)\..*$/, "$1");
 
 	return Object.keys(mdFiles)
 		.map(path => new Object({
@@ -8,40 +10,18 @@ const rootPages = () => {
 			slug: getSlug(path),
 			path: `/${getSlug(path)}`,
 			title: mdFiles[path].metadata?.title || getSlug(path),
+			date: mdFiles[path].metadata?.date[0] ? new Date(mdFiles[path].metadata.date[0]) : null,
 			metadata: mdFiles[path].metadata,
-		}))
-		.filter(file => ![`index`].includes(file.slug))
-}
+		}));
+};
 
 export async function get({ params }) {
-	const { slug } = params
-	const pages = rootPages()
-	const getProp = (obj, prop, i=null) => {
-		const val = prop.split('.').reduce((acc, c) => acc && acc[c], obj)
-		return val && typeof i === `number` ? val[i] : val
-	}
-	const listPosts = (list, hasProp=1) => (prop, i) => !hasProp ?
-		list
-			.filter(obj => !getProp(obj, prop, i))
-		: list
-			.filter(obj => getProp(obj, prop, i))
-			.sort((a, b) => organize(a, b, prop, i))
-	const organize = (a, b, prop, i) => {
-		const valA = getProp(a, prop, i)
-		const valB = getProp(b, prop, i)
+	const { slug } = params;
+	const pages = rootPages();
 
-		if (valA instanceof Date)
-			return valA - valB
-		if (new Date(valA) instanceof Date)
-			return new Date(valA) - new Date(valB)
-		if (typeof valA === `string`)
-			return valA.localCompare(valB)
-		return 0
-	}
-
-	return { status: 200, body: [
-		...listPosts(pages)(`metadata.date`, 0).reverse(),
-		...listPosts(pages, false)(`metadata.date`)
-			.sort((a, b) => organize(a, b, `title`))
-	]}
+	return {
+		status: 200,
+		body: getPostsFrom(pages)([`date`, `title`]),
+		// body: listPosts(pages)([[`metadate.date`, 0], `title`])
+	};
 }
