9cda329f144bd2dc9772e01b848305bfcd0c4ffc
Johan S. Steinberg <steinberg@hey.com>
2022-02-08 09:44:20 +0100

    Update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,29 +1,22 @@
-import { mdsvex } from "mdsvex";
-import mdsvexConfig from "./mdsvex.config.js";
-import preprocess from "svelte-preprocess";
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: [".svelte", ...mdsvexConfig.extensions],
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
 
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: "#svelte",
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		})
-  },
+		adapter: adapter()
+	},
 
 	preprocess: [
 		preprocess({
-			postcss: true,
+			postcss: true
 		}),
-		mdsvex(mdsvexConfig),
-	],
+		mdsvex(mdsvexConfig)
+	]
 };
 
 export default config;
