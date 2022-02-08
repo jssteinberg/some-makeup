d8b5e1941cf363643a981d1395e13bec72b04037
Johan S. Steinberg <steinberg@hey.com>
2022-02-08 09:32:13 +0100

    Update eslint and add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,11 @@
 module.exports = {
 	root: true,
-	extends: ['eslint:recommended'],
+	extends: ['eslint:recommended', 'prettier'],
 	plugins: ['svelte3'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2019
+		ecmaVersion: 2020
 	},
 	env: {
 		browser: true,
