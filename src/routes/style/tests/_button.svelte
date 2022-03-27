590a483533fc0ffd694893e25d65ca5aeada6c71
Johan S. Steinberg <steinberg@hey.com>
2022-03-27 19:34:49 +0200

    feat(some): more tests

--- src/routes/style/tests/_button.svelte
+++ src/routes/style/tests/_button.svelte
@@ -6,14 +6,16 @@
 
 	<p>A <button>button</button> is difficult to use out of the box inline. It has a lot of default browser styling, and for most sites that use buttons, they are styled specifically. How much does a <button>button</button> need to be fixed?</p>
 
-	<p>It works more as block level element it looks nicer:</p>
+	<p>It works better out of the box in a block without normal text:</p>
 
 	<button>Button</button>
 </section>
 
 <style>
-	.button button {
+	:is(p,li,dl,blockquote,h1,h2,h3,h4,h5,h6) button { /* address? */
+		border: none;
 		padding-block-start: 0;
 		padding-block-end: 0;
+		font-size: 1em;
 	}
 </style>
--- src/routes/style/tests/index.svelte
+++ src/routes/style/tests/index.svelte
@@ -1,1 +1,11 @@
 <h1>Tests</h1>
+
+<h2><code>address</code></h2>
+
+<p>Contact the author of this page:</p>
+
+<address>
+  <a href="mailto:johan@rocks.world">johan@rocks.world</a><br>
+  <a href="tel:+13115552368">(311) 555-2368</a><br>
+  <button>Go &rarr;</button>
+</address>
