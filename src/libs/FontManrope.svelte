bcbe0da40b478e003dfb99ab3404d966bcd7624c
Steinberg <steinberg@hey.com>
2022-04-07 22:42:35 +0200

    change font loading to script import

--- src/libs/FontManrope.svelte
+++ src/libs/FontManrope.svelte
@@ -1,19 +1,3 @@
-<div aria-hidden="true">
-	<style>
-		@supports (font-variation-settings: normal) {
-			@font-face {
-				font-family: "Manrope";
-				src:
-					url("/manrope/Manrope[wght].4.505.woff2") format("woff2 supports variations"),
-					url("/manrope/Manrope[wght].4.505.woff2") format("woff2-variations");
-				font-weight: 100 1000;
-				font-stretch: 25% 151%;
-				font-style: normal;
-			}
-		}
-	</style>
-</div>
-
-<svelte:head>
-	<link rel="preload" href="/manrope/Manrope[wght].4.505.woff2" as="font" type="font/woff2">
-</svelte:head>
+<script>
+	import "/static/manrope/font.css";
+</script>
