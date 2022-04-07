5b672075a41fe22d15dd15747d6752bdcd7601c0
Steinberg <steinberg@hey.com>
2022-04-07 22:02:42 +0200

    fix manrope font loading

--- src/libs/FontManrope.svelte
+++ src/libs/FontManrope.svelte
@@ -1,6 +1,16 @@
 <div aria-hidden="true">
-	<style lang="scss">
-		@import '../../static/manrope/font.css';
+	<style>
+		@supports (font-variation-settings: normal) {
+			@font-face {
+				font-family: "Manrope";
+				src:
+					url("/manrope/Manrope[wght].4.505.woff2") format("woff2 supports variations"),
+					url("/manrope/Manrope[wght].4.505.woff2") format("woff2-variations");
+				font-weight: 100 1000;
+				font-stretch: 25% 151%;
+				font-style: normal;
+			}
+		}
 	</style>
 </div>
 
