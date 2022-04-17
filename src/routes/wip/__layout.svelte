b526d662918733f22777f61e7ec537fdc991b340
Steinberg <steinberg@hey.com>
2022-04-17 22:46:12 +0200

    noindex on /wip

--- src/routes/wip/__layout.svelte
+++ src/routes/wip/__layout.svelte
@@ -2,6 +2,10 @@
 	import { page } from '$app/stores';
 </script>
 
+<svelte:head>
+	<meta name="robots" content="noindex">
+</svelte:head>
+
 <div class={$page.url.pathname.replace(/^\//, '').replace(/\//g, '-') || 'home'}>
 	<slot />
 </div>
