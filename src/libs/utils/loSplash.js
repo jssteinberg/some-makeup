ba4394df1ab8fe92574138c2bd7b5e85d6744444
Steinberg <steinberg@hey.com>
2022-05-26 13:45:26 +0200

    doc(some): minor example for loSplash

--- src/libs/utils/loSplash.js
+++ src/libs/utils/loSplash.js
@@ -1,19 +1,25 @@
 /* loSplash.js - A splash of FP util functions. */
 
 /** Fn returns an object property's value from a key string.
+ *
+ * ```
+ * getValue(obj, key, valArrIndex))
+ * ```
+ *
  * @arg {object} obj
  * @arg {string} key - `parentKey.key`. Use `.` to separate keys.
  * @arg {number} [i] - index of value to return, if value is array.
  * @return {any}
  */
 export const getValue = (obj, key, i = null) => {
-	const val = key.split('.').reduce((acc, c) => acc && acc[c], obj);
+  const val = key.split('.').reduce((acc, c) => acc && acc[c], obj)
 
-	return val && typeof i === `number` ? val[i] : val;
-};
+  return val && typeof i === `number` ? val[i] : val
+}
+
 
 /** Fn returns an immutable object from object.
  * @arg {object} obj
  * @return {object}
  */
-export const factory = (obj) => Object.freeze(obj);
+export const factory = (obj) => Object.freeze(obj)
