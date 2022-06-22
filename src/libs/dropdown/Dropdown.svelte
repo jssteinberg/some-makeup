6f25708d97085c4ed00cd20f01e9de0fc58e9a29
Steinberg <steinberg@hey.com>
2022-06-22 18:40:59 +0200

    feat(some): update all; start dropdown

---
+++ src/libs/dropdown/Dropdown.svelte
@@ -0,0 +1,21 @@
+<div class="dropdown-wrapper">
+	<input
+		id="dropdown-toggle"
+		type="checkbox"
+		value="toggle"
+		aria-controls="dropdown-content"
+		aria-expanded="true"
+	/>
+	<label for="dropdown-toggle">Toggle</label>
+
+	<div id="dropdown-content">
+		<slot />
+	</div>
+</div>
+
+<style>
+	.dropdown-wrapper:not(:focus-within) #dropdown-content,
+	#dropdown-toggle:not(:checked) ~ #dropdown-content {
+		display: none;
+	}
+</style>
---
+++ src/routes/wip/a11y-full-universal-dropdown.md
@@ -0,0 +1,16 @@
+---
+date: ['2022-06']
+title: Dropdown
+---
+
+# Possible A11y Fully Featured Universal Dropdown
+
+<Dropdown>
+	
+Hi!
+
+</Dropdown>
+
+<script>
+import Dropdown from '$libs/dropdown/Dropdown.svelte'
+</script>
