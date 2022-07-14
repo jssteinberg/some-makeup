87cbb0bac56b9ff28b320d0bd9caf47e5c8e305d
Steinberg <steinberg@hey.com>
2022-07-14 10:57:28 +0200

    feat(some): only run plausible on main domain

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,15 +1,22 @@
 <script>
+	import { onMount } from "svelte";
 	import { prod } from "$app/env";
 	import "../libs/app.css";
 	import { page } from "$app/stores";
 	import Header from "/src/libs/Header.svelte";
 	import Footer from "/src/libs/Footer.svelte";
+
+	let prodHref = false;
+
+	onMount(() => {
+		prodHref = !!window?.location.href.match(/\/\/some.makeup/);
+	});
 </script>
 
 <svelte:head>
 	<link rel="canonical" href={`https://some.makeup${$page.url.pathname}`} />
 
-	{#if prod}
+	{#if prod && prodHref}
 		<!-- Plausible -->
 		<script
 			defer
