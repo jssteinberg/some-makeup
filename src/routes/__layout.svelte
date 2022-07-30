7a18f0c9e8fa0d70abc921294f0ec2b90fe5789f
Johan S. Steinberg <steinberg@hey.com>
2022-07-30 10:01:15 +0200

    refactor(some): remove plausible script

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,28 +1,12 @@
 <script>
-	import { onMount } from "svelte";
-	import { prod } from "$app/env";
 	import "../libs/app.css";
 	import { page } from "$app/stores";
 	import Header from "/src/libs/Header.svelte";
 	import Footer from "/src/libs/Footer.svelte";
-
-	let prodHref = false;
-
-	onMount(() => {
-		prodHref = !!window?.location.href.match(/\/\/some.makeup/);
-	});
 </script>
 
 <svelte:head>
 	<link rel="canonical" href={`https://some.makeup${$page.url.pathname}`} />
-
-	{#if prod && prodHref}
-		<!-- Plausible -->
-		<script
-			defer
-			data-domain="some.makeup"
-			src="https://plausible.io/js/plausible.js"></script>
-	{/if}
 </svelte:head>
 
 <div id="document">
