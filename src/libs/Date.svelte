4794b82d1b059003a9157d5689078ec4781a0ef9
Steinberg <steinberg@hey.com>
2022-09-18 08:24:38 +0200

    fix(some): missing if

--- src/libs/Date.svelte
+++ src/libs/Date.svelte
@@ -2,15 +2,17 @@
 	export let date;
 </script>
 
-<p class="date-info">
-	<em>
-		{#if date.length > 1}
-			(Up)date ({date[date.length - 1]}) {date[0]}
-		{:else}
-			Date {date[0]}
-		{/if}
-	</em>
-</p>
+{#if date}
+	<p class="date-info">
+		<em>
+			{#if date.length > 1}
+				(Up)date ({date[date.length - 1]}) {date[0]}
+			{:else}
+				Date {date[0]}
+			{/if}
+		</em>
+	</p>
+{/if}
 
 <style>
 	.date-info {
