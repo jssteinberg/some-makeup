cb17280cb0d31ad971a5b6a72d8e200bbea29cf0
Johan S. Steinberg <steinberg@hey.com>
2022-10-08 20:12:41 +0200

    fix(some): style for hr width calc

--- src/libs/Hr.svelte
+++ src/libs/Hr.svelte
@@ -1,20 +1,23 @@
 <!-- Example
- <Hr --hr-width="var(--content-width)" /> -->
+
+```
+<Hr --hr-width="100%" />
+``` -->
+
 <hr />
 
 <style>
 	hr {
-		--_content-width: var(--hr-width, var(--content-width, 100%));
-		--_base-size: max(var(--TOUCH-TARGET-SIZE) / 3, var(--space, 1em));
+		--_content-width: min(100%, var(--hr-width, var(--content-width, 100%)));
 		--_size: max(var(--TOUCH-TARGET-SIZE), 1.5 * var(--space, 1em));
 
 		all: unset;
-		box-shadow: inset 0 0 0 1px var(--hr-fg);
+		box-shadow: inset 0 0 0 1px var(--hr-fg, currentColor);
 		display: block;
 		width: var(--_size);
 		height: var(--_size);
 		margin: calc(4 * var(--space, 1em))
-			calc(var(--_content-width) * 0.5 - var(--_base-size) * 0.5);
+			calc(var(--_content-width) * 0.5 - var(--_size) * 0.5);
 		transform: rotate(45deg);
 	}
 </style>
