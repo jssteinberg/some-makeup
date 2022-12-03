5f604da1de948c61840022bab241df1586c1e148
Steinberg <steinberg@hey.com>
2022-12-03 21:04:08 +0100

    feat(some): mv away from post util fn

--- src/routes/api/posts/+server.js
+++ src/routes/api/posts/+server.js
@@ -1,8 +1,20 @@
-import { fetchMarkdownPosts } from '$libs/utils/fetchMarkdownPosts';
 import { json } from '@sveltejs/kit';
 
 export const GET = async () => {
-	const allPosts = await fetchMarkdownPosts();
+	const allPostFiles = import.meta.glob('/src/routes/*.md');
+	const iterablePostFiles = Object.entries(allPostFiles);
+
+	const allPosts = await Promise.all(
+		iterablePostFiles.map(async ([path, resolver]) => {
+			const { metadata } = await resolver();
+			const postPath = path.slice(11, -3);
+
+			return {
+				meta: metadata,
+				path: postPath,
+			};
+		})
+	);
 
 	// const sortedPosts = allPosts.sort((a, b) => {
 	// 	return new Date(b.meta.date[0]) - new Date(a.meta.date[0]);
