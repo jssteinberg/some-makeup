d4c1710ed6ff24a4ea7586d2e31f89e719e498ad
Steinberg <steinberg@hey.com>
2022-12-04 22:34:48 +0100

    refactor(some): formatting

--- src/libs/utils/loSplash.js
+++ src/libs/utils/loSplash.js
@@ -1,4 +1,4 @@
-/* loSplash.js - A splash of FP util functions. */
+/* loSplash.js -- A splash of FP util functions. */
 
 /** Fn returns an object property value from a key string.
  *
@@ -12,10 +12,12 @@
  * @return {any}
  */
 export const getValue = (obj, key, i = null) => {
-  const val = key.split('.').reduce((acc, c) => acc && acc[c], obj)
+	const val = key.split('.').reduce((acc, c) => acc && acc[c], obj);
 
-  return val && typeof i === `number` ? val[i] : val
-}
+	return val && typeof i === `number`
+		? key.split('.').reduce((acc, c) => acc && acc[c], obj)[i]
+		: val;
+};
 
 
 /** Fn returns an immutable object from object.
