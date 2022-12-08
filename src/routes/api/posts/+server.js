ff39c87910371eab99de7930b71d9844534c9cd7
Steinberg <steinberg@hey.com>
2022-12-08 21:55:16 +0100

    fix(some): post sorting of date (forgotten)

--- src/routes/api/posts/+server.js
+++ src/routes/api/posts/+server.js
@@ -16,9 +16,12 @@ export const GET = async () => {
 		})
 	);
 
-	// const sortedPosts = allPosts.sort((a, b) => {
-	// 	return new Date(b.meta.date[0]) - new Date(a.meta.date[0]);
-	// });
 
-	return json(allPosts.filter(post => !post.path.match(/\+page/gi)));
+	return json(
+		allPosts
+			.filter(post => !post.path.match(/\+page/gi))
+			.sort((a, b) => {
+				return new Date(b.meta.date[0]) - new Date(a.meta.date[0]);
+			})
+	);
 }
