67bb2fda294ea019c91860fc19071234e4536901
Steinberg <steinberg@hey.com>
2023-01-01 11:49:40 +0100

    refactor: can postcss be simplified?
    vite should minify by default. autoprefixer looked double...

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,16 +1,9 @@
 const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
 
 const mode = process.env.NODE_ENV;
-const dev = mode === 'development';
 
 const config = {
 	plugins: [
-		autoprefixer(),
-		!dev &&
-			cssnano({
-				preset: 'default'
-			}),
 		autoprefixer
 	]
 };
