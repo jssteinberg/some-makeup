dff5aa253222de18b51966c7217db26a1daa7590
jssteinberg <steinberg@hey.com>
2023-01-03 22:19:52 +0100

    feat(some): set postcss preset env config

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,11 +1,12 @@
-// const autoprefixer = require('autoprefixer');
 const postcssPresetEnv = require('postcss-preset-env');
 
 const config = {
-	// plugins: [autoprefixer]
 	plugins: [postcssPresetEnv({
-		stage: 3, // 4 is stable
-		minimumVendorImplementations: 2 // number of browser that has implemented the feature
+		stage: 3,
+		minimumVendorImplementations: 1, // number of browser that has implemented the feature
+		features: {
+			"logical-properties-and-values": true,
+		},
 	})]
 };
 
