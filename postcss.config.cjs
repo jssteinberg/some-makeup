14c871eba3f9aad4856e8e56f143de9a83a593d6
jssteinberg <steinberg@hey.com>
2023-01-03 23:31:18 +0100

    chore(some): finish postcss; add robots.txt

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,10 +2,9 @@ const postcssPresetEnv = require('postcss-preset-env');
 
 const config = {
 	plugins: [postcssPresetEnv({
-		stage: 3,
-		minimumVendorImplementations: 1, // number of browser that has implemented the feature
+		// stage: false,
 		features: {
-			"logical-properties-and-values": true,
+			"logical-properties-and-values": { dir: "ltr" },
 		},
 	})]
 };
