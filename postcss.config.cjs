bcc5beaab5b18ffe9c8911ece42ade5aa58d4afd
jssteinberg <steinberg@hey.com>
2023-01-17 09:43:58 +0100

    feat(some): no postcss case insensitive polyfill

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -4,7 +4,9 @@ const config = {
 	plugins: [postcssPresetEnv({
 		// stage: false,
 		features: {
+			"postcss-attribute-case-insensitive": false,
 			"logical-properties-and-values": { dir: "ltr" },
+			"oklab-function": { subFeatures: { displayP3: false } },
 		},
 	})]
 };
