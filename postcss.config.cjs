883e1eb9a44f26c30c7d53669a8ac48ba7d1dc07
Steinberg <steinberg@hey.com>
2023-02-08 00:21:14 +0100

    chore(some)!: leave css custom prop

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -4,6 +4,7 @@ const config = {
 	plugins: [postcssPresetEnv({
 		features: {
 			"case-insensitive-attributes": false,
+			"custom-properties": false,
 			"logical-properties-and-values": { dir: "ltr" },
 			// "oklab-function": { subFeatures: { displayP3: false } },
 		},
